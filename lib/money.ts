66fbf6b94e663ca0b86785db20e7a03ce4d744ed
Mohmedahmed888 <dbdn496@gmail.com>
2026-03-08 00:15:44 +0200

    update admin login and database fixes

--- lib/money.ts
+++ lib/money.ts
@@ -1,4 +1,4 @@
 export function formatEGP(cents: number) {
   const egp = cents / 100
-  return new Intl.NumberFormat('ar-EG', { style: 'currency', currency: 'EGP' }).format(egp)
+  return new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'EGP' }).format(egp)
 }
